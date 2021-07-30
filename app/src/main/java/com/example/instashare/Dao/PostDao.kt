4b3c13f607a059c717835489ff79a7517a59d4e2
vickyy044 <43821289+vickyy044@users.noreply.github.com>
2021-07-30 11:20:32 +0530

    - Added postAdapter display posts on MainActivity

--- app/src/main/java/com/example/instashare/Dao/PostDao.kt
+++ app/src/main/java/com/example/instashare/Dao/PostDao.kt
@@ -2,7 +2,9 @@ package com.example.instashare.Dao
 
 import com.example.instashare.models.Post
 import com.example.instashare.models.User
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.GlobalScope
@@ -23,6 +25,26 @@ class PostDao {
             val currentTime = System.currentTimeMillis()
             val post = Post(text, user, currentTime)
             postCollection.document().set(post)
+        }
+    }
+
+    fun getPostById(postId: String): Task<DocumentSnapshot> {
+        return postCollection.document(postId).get()
+    }
+    fun updateLike(postId: String){
+        GlobalScope.launch {
+            val currentUser = Firebase.auth.currentUser!!.uid
+            val post = getPostById(postId).await().toObject(Post::class.java)!!
+
+            val isLiked = post.likedBy.contains(currentUser)
+
+            if(isLiked){
+                post.likedBy.remove(currentUser)
+            }else{
+                post.likedBy.add(currentUser)
+            }
+
+            postCollection.document(postId).set(post)
         }
     }
 }
--- app/src/main/java/com/example/instashare/MainActivity.kt
+++ app/src/main/java/com/example/instashare/MainActivity.kt
@@ -1,22 +1,57 @@
 package com.example.instashare
 
+import android.app.DownloadManager
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.instashare.Dao.PostDao
+import com.example.instashare.models.Post
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.firestore.Query
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), IPostAdapter {
+    private lateinit var adapter: PostAdapter
+    private lateinit var postDao: PostDao
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("MainActivity","inside oncreate")
+        Log.d("MainActivity","inside onCreate")
 
 
         floatingActionBt.setOnClickListener {
             val intent = Intent(this, CreatePost::class.java )
             startActivity(intent)
-
         }
+
+        setUpRecyclerView()
+    }
+
+    private fun setUpRecyclerView() {
+        postDao = PostDao()
+        val postsCollections = postDao.postCollection
+        val query = postsCollections.orderBy("createdBy", Query.Direction.DESCENDING)
+        val recyclerViewOptions = FirestoreRecyclerOptions.Builder<Post>().setQuery(query,Post::class.java).build()
+        adapter = PostAdapter(recyclerViewOptions,this)
+
+        recyclerView.adapter=adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        adapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        adapter.stopListening()
+    }
+
+    override fun onLikeClicked(postId: String) {
+        postDao.updateLike(postId)
     }
 }
